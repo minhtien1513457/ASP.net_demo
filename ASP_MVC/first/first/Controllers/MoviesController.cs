68ef0476d55711d5c1d8dc58c9d204ba0d87852f
TienPhan <minhtienit1997@gmail.com>
2018-09-11 00:45:09 +0700

    asp mvs

---
+++ ASP_MVC/first/first/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using first.Models;
+using Quobject.SocketIoClientDotNet.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime;
+
+namespace first.Controllers
+{
+	public class MoviesController : Controller
+	{
+		//
+		// GET: /Movie/Random
+		public ActionResult Random()
+		{
+			var movie = new Movie() { Name = "Harry" };
+			var socket = IO.Socket("http://localhost:3000");
+			socket.On(Quobject.SocketIoClientDotNet.Client.Socket.EVENT_CONNECT, () =>
+			{
+				socket.Emit("client-send-data", "trungbede");
+				socket.On("server-send-data", (data) =>
+				{
+					//movie.Name = "aaa";
+					movie.Name = data.ToString();
+					//socket.Disconnect();
+				});				
+			});
+			return View(movie);
+		}
+	}
+}
---
+++ ASP_MVC/first/first/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Sockets;
+using Quobject.SocketIoClientDotNet.Client;
+namespace first.Models
+{
+    public class Movie
+    {
+        public int Id { set; get; } 
+        public string Name { set; get; }
+        public void connectServer()
+        {
+             var socket = IO.Socket("http://localhost:3000");
+            socket.On(Quobject.SocketIoClientDotNet.Client.Socket.EVENT_CONNECT, () =>
+            {
+                socket.Emit("hi");
+
+            });
+
+            socket.On("hi", (data) =>
+            {
+                Console.WriteLine(data);
+                socket.Disconnect();
+            });
+            Console.ReadLine();
+        }
+    }
+}
